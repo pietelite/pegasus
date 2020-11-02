8700cadbf4025cde3f5f5b7672e4fe47fdaf9577
Pieter Svenson <pieter.svenson@outlook.com>
2020-11-02 16:12:31 -0500

    removed line that manually creates database schema

--- pegasus/settings.py
+++ pegasus/settings.py
@@ -109,7 +109,6 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 }
-init_database()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
